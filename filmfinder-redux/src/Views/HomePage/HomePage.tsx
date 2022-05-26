454ac301ea75dbc51fea0930f107b2d45b76d7eb
bayodes <bayode.o.olaoye@gmail.com>
2022-05-26 06:53:13 -0400

    More Homepage work

--- filmfinder-redux/src/Views/HomePage/HomePage.tsx
+++ filmfinder-redux/src/Views/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar } from "../../Components/Navbar/Navbar";
+import "./HomePage.css";
 
 export const HomePage: React.FC = () => {
   return (
--- filmfinder-redux/src/Views/RegisterPage/RegisterPage.tsx
+++ filmfinder-redux/src/Views/RegisterPage/RegisterPage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./RegisterPage.css";
 import { RegisterForm } from "../../Components/RegisterForm/RegisterForm";
 
 export const RegisterPage: React.FC = () => {
