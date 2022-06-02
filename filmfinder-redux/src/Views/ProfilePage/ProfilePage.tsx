f511a0daa6ead1e25a047815ec891988ffc06998
bayodes <bayode.o.olaoye@gmail.com>
2022-06-02 14:41:45 -0400

    refactor

--- filmfinder-redux/src/Views/ProfilePage/ProfilePage.tsx
+++ filmfinder-redux/src/Views/ProfilePage/ProfilePage.tsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../Store";
 import { NavbarLoggedIn } from "../../Components/Navbar/NavbarLoggedIn";
 import { Footer } from "../../Components/Footer/Footer";
-import "../../avatar.jpg";
+import "../../Assets/avatar.jpg";
 
 export const ProfilePage: React.FC = () => {
   const currUser = useSelector((state: RootState) => state.user);
@@ -12,16 +12,6 @@ export const ProfilePage: React.FC = () => {
     <div>
       <NavbarLoggedIn />
 
-      <div className="profile-card">
-        <div className="card-header">
-          <div className="pic">
-            <img src="avatar.jpg" alt="" />
-          </div>
-          <div className="name">
-            {currUser.user?.firstName} {currUser.user?.lastName}
-          </div>
-        </div>
-      </div>
       <Footer />
     </div>
   );
